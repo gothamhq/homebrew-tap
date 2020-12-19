66b38040b79fddbf5be54a5b2458bb507296b3f0
GoReleaser <GoReleaser@GothamHQ.com>
2020-12-19 16:06:26 -0500

    Brew formula update for gotham version v0.10.1

--- Formula/gotham.rb
+++ Formula/gotham.rb
@@ -2,12 +2,12 @@
 class Gotham < Formula
   desc "An awesome static site generator based on Hugo."
   homepage "https://GothamHQ.com"
-  version "0.10.0"
+  version "0.10.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gothamhq/gotham/releases/download/v0.10.0/gotham-v0.10.0-macos-amd64.tar.gz"
-    sha256 "7755d7b2fe9c95cf64528468e92437d4113a5f1cbfe7af151309a780ac1f8a67"
+    url "https://github.com/gothamhq/gotham/releases/download/v0.10.1/gotham-v0.10.1-macos-amd64.tar.gz"
+    sha256 "a00b3a30ba919b0504d58c0a4cd9382935a7f05e28da7c1b90a8a0e522a3b5dc"
   end
 
   depends_on "go"
